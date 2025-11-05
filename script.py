3f4aba1edfbc631616f15fa7f163fd4e507b809d
Ehsan khalili <er.roustaei@gmail.com>
2025-11-05 14:42:40 +0330

    add script.py file

---
+++ script.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request, make_response
+from functools import wraps
+
+app = Flask(__name__)
+
+USERS = {"admin": "1234"} # chenge info
+
+def check_auth(username, password):
+    return username in USERS and USERS[username] == password
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            response = make_response(jsonify({"error": "Unauthorized"}), 401)
+            response.headers['WWW-Authenticate'] = 'Basic realm="Login Required"'
+            return response
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/', methods=['GET'])
+@requires_auth
+def hi():
+    return jsonify({"message": "hello world!!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
