11dd5e0c2182e8f9bafa5e467a7c26de27a50bce
Ehsan khalili <er.roustaei@gmail.com>
2025-11-07 20:06:36 +0330

    Bug fixes request-to-api.js file

--- request-to-api.js
+++ request-to-api.js
@@ -1,18 +1,33 @@
-function req() {
+async function req(isReload=Boolean) {
     try {
-        const auth = 'Basic ' + btoa('admin' + ':' + '1234');
-        const res = await fetch('http://localhost:5000/hi', {
+        const auth = 'Basic ' + btoa('admin:1234');
+        const res = await fetch('http://localhost:5000/', {
             method: 'GET',
             headers: {
                 'Authorization': auth,
                 'Content-Type': 'application/json'
-            },
+            }
         });
-        if (!res.ok) throw new Error(`Error ${res.status}`);
+
+        if (!res.ok) {
+            throw new Error(`HTTP Error: ${res.status} ${res.statusText}`);
+        }
+
         const data = await res.json();
-        return JSON.stringify(data);
+        return data;
     } catch (e) {
-        console.log(e.message);
-    
+        if (e.name == "TypeError" && e.message == "Failed to fetch") {
+            let Ir = null
+            if (isReload == true) {Ir=true} else if (isReload == false) {Ir=false}
+            return re_req(Ir)
+        }
+        console.log(e.message)
+        throw 'خطا در درخواست:', e;
     }
+}
+
+function re_req(isReload) {
+    setTimeout(async () => {
+        if(isReload) {window.location.reload();}
+    }, 1000)
 }
